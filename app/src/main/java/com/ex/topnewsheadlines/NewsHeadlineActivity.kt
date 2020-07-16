2685e2059b3b023fdd674ed69a3fff53032a95ad
sangeethakanagarajan <sangetamil@gmail.com>
2020-07-16 11:52:26 +0530

    News headlines - set up design in xml

--- app/src/main/java/com/ex/topnewsheadlines/NewsHeadlineActivity.kt
+++ app/src/main/java/com/ex/topnewsheadlines/NewsHeadlineActivity.kt
@@ -3,9 +3,9 @@ package com.ex.topnewsheadlines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NewsHeadlineActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news_headline)
     }
 }
